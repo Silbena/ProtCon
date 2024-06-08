03d97c4b107fe73e93540379e84c5a08afc1c0fe
71930667+Silbena@users.noreply.github.com <71930667+Silbena@users.noreply.github.com>
2024-06-08 14:55:09 +0200

    final converters.py

--- converters_app/converters/converters.py
+++ converters_app/converters/converters.py
@@ -1,8 +1,18 @@
 import converters.csvConverter as csv
+import converters.fasta_embl as fe
+import converters.fasta_gb as fg
+import converters.gb_embl as ge
+
 # Add the rest of converters
 
 def get_converters() -> list:
     return [
         csv.CsvToTsv,
-        csv.TsvToCsv
+        csv.TsvToCsv,
+        fg.GbToFasta,
+        fg.FastaToGb,
+        fe.EmblToFasta,
+        fe.FastaToEmbl,
+        ge.EmblToGb,
+        ge.GbToEmbl
         ]
